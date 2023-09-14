501bffb9445f1b173c0804687f7287b5ba0ccee8
janvi zala <133522081+janvizala@users.noreply.github.com>
2023-09-14 10:23:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <script src='https://cdn.scaledrone.com/scaledrone.min.js'></script>
+    <script type="text/javascript" src="script.js"></script>
+    <style>
+    .header{
+    height: 100px;
+    border: 1px solid black;
+    background-color: black;
+
+    }
+    h1{
+        color: white;
+    }
+    .body{
+        height: 550px;
+        border: 1px solid black;
+    }
+    .footer{
+        height: 80px;
+        border: 1px solid black;
+        background-color: black;
+    }
+    .From{
+        height: 200px;
+        width: 250px;
+        border: 1px solid black;
+        margin-top: 180px;
+        margin-left: 200px;
+        border-radius: 10px;
+        float: left;
+    }
+    .To{
+        height: 200px;
+        width: 250px;
+        border: 1px solid black;
+        margin-top: 180px;
+        margin-right: 200px;
+        border-radius: 10px;
+        margin-left: 500px;
+        float: left;
+    }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1 align="center">Video Call</h1>
+    </div>
+    <div class="body">
+        <video class="From" id="localVideo" autoplay muted></video>
+        <video class="To" id="remoteVideo" autoplay></video>
+    </div>
+    <div class="footer">
+        <h1 align="center">@copyright</h1></h1>
+    </div>
+
+</body>
+</html>
+<!-- qKcp0PdxU5XEDXes -->
+<!-- DI04cadqB1LBAy5dCMQo3SUg8cpIXgYD -->
+<!-- AY2540 -->
+<!-- ay2540.infinityfreeapp.com -->
